1675d808630563efb7ce48d2df44a68c8fd1be45
jdlrobson <jdlrobson@gmail.com>
2017-08-11 16:09:13 -0500

    Don't try to guess sights when Wikipedia link found
    This updates logic of extract-sights-from-text. Sights do not
    need to be guessed if they are indicated by Wikipedia link.

--- libs/server/endpoints/voyager/extract-sights-from-text.js
+++ libs/server/endpoints/voyager/extract-sights-from-text.js
@@ -1,16 +1,25 @@
 import extractBoldItems from './extract-bold-items'
 import { extractElements, extractElementsTextContent } from './domino-utils'
+import getParentWithTag from './get-parent-with-tag'
 
 export default function ( text ) {
-  var sights = extractBoldItems( text );
+  let sights = [];
   // sister site links e.g. Panama City
   const sisterSiteLink = extractElements( text, '.listing-sister a', true )
   if ( sisterSiteLink.extracted.length ) {
     sisterSiteLink.extracted.forEach( ( sister ) => {
       sights.push( sister.getAttribute( 'href' ).replace( './W:', '' ).replace( /_/g, ' ' ) );
+      const listItem = getParentWithTag( sister, 'LI' );
+      if ( listItem && listItem.parentNode ) {
+        listItem.parentNode.removeChild( listItem );
+      }
     } );
   }
+
+  text = sisterSiteLink.document.body.innerHTML;
   sights = sights.concat(
+    extractBoldItems( text )
+  ).concat(
     extractElementsTextContent( extractElements( text, 'a', true ).extracted )
   );
   return sights;
